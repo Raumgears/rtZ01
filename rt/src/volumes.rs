2e52f80eeff3dd00926ed9b531aa1caf1e19714a
damien guimiot <damienguimiot@gmail.com>
2025-12-10 16:41:46 +0100

    cylinder functionnal, missing caps

--- rt/src/volumes.rs
+++ rt/src/volumes.rs
@@ -4,6 +4,7 @@ mod cube;
 mod plane;
 mod subplane_d;
 mod subplane_s;
+mod cylinder;
 
 pub use hittable_list::*;
 pub use sphere::*;
@@ -11,3 +12,4 @@ pub use cube::*;
 pub use plane::*;
 pub use subplane_d::*;
 pub use subplane_s::*;
+pub use cylinder::*;
