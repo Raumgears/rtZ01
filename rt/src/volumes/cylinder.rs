44b56f64119b936c593f12ce6ae13d75fb382392
alebourg <abelux662@gmail.com>
2025-12-18 13:27:12 +0100

    Cylinder with rayon and test

--- rt/src/volumes/cylinder.rs
+++ rt/src/volumes/cylinder.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::sync::Arc;
 
 use crate::traits::{HitRecord, Hittable, Material};
 use crate::basics::{Point3, Ray, Vec3, dot, unit_vec};
@@ -8,11 +8,11 @@ pub struct Cylinder {
     pub length: f64,
     pub radius: f64,
     pub orientation: Vec3,
-    pub mat: Rc<dyn Material>,
+    pub mat: Arc<dyn Material>,
 }
 
 impl Cylinder {
-    pub fn new(center: Point3, length: f64, radius: f64, orientation: Vec3, mat: Rc<dyn Material>) -> Self {
+    pub fn new(center: Point3, length: f64, radius: f64, orientation: Vec3, mat: Arc<dyn Material>) -> Self {
         Self{center, length, radius, orientation: unit_vec(orientation), mat}
     }
 }
@@ -20,10 +20,10 @@ impl Cylinder {
 impl Hittable for Cylinder {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
         let x = ray.origin() - self.center;
-        let a = ray.direction().length_squared() - (dot(ray.direction(), self.orientation)).powf(2.);   
+        let a = ray.direction().length_squared() - (dot(ray.direction(), self.orientation)).powf(2.0);
         let half_b = dot(ray.direction(), x) - (dot(ray.direction(), self.orientation) * dot(x, self.orientation));
-        let c = x.length_squared() - dot(x, self.orientation).powf(2.) - self.radius * self.radius;
-    
+        let c = x.length_squared() - dot(x, self.orientation).powf(2.0) - self.radius * self.radius;
+
         let discriminant = half_b * half_b - a * c;
         if discriminant < 0.0 {
             return false;
